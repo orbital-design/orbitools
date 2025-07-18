b53a271a79a41153bf9123efcaf1c1a56bf1d4b2
addzycullen <adam.cullen@live.co.uk>
2025-07-18 19:12:40 +0100

    Fixed bug:
    The key issue was that isset() returns false for null values, but
      array_key_exists() will return true if the key exists, even if its value is null.
    
      Now:
      1. Menu Groups has 'configure_url' => null
      2. array_key_exists() detects that the key exists
      3. config_url gets set to null
      4. ! empty($module['config_url']) will be false for null
      5. Settings button won't show
    
      The settings button should now be hidden for the Menu Groups module!

--- inc/Admin/adminkit/fields/modules/Orbitools_Modules_Field.php
+++ inc/Admin/adminkit/fields/modules/Orbitools_Modules_Field.php
@@ -104,11 +104,11 @@ class Orbitools_Modules_Field extends Orbitools\AdminKit\Field_Base
 
         // Add config URLs to each module (only if not already provided)
         foreach ($modules as $module_id => &$module) {
-            if (isset($module['configure_url'])) {
+            if (array_key_exists('configure_url', $module)) {
                 // Use the configure_url provided by the module (even if null or empty)
                 $module['config_url'] = $module['configure_url'];
             } elseif (empty($module['config_url'])) {
-                // Generate default config URL only if no configure_url is set
+                // Generate default config URL only if no configure_url key is set
                 $module['config_url'] = $this->get_module_config_url($module_id);
             }
         }
