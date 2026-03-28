5f4a0edf9bf99ec1f8427a630dd26062eb7b5e94
addzycullen <adam.cullen@live.co.uk>
2026-03-28 03:32:04 +0000

    Release v1.3.11

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '669c7ca13f9650f21998e792801d61c23a90c7e9',
+        'reference' => '254e17b95500887cdee9cfc2e33d8c99fadd862f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '669c7ca13f9650f21998e792801d61c23a90c7e9',
+            'reference' => '254e17b95500887cdee9cfc2e33d8c99fadd862f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
